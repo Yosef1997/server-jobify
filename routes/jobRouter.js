fb411a60120d661539c6e03f39d41e04d5d10fcd
JanBu <yosefsitumorang97@gmail.com>
2025-05-20 21:48:25 +0700

    feat: validate job

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -21,7 +21,7 @@ router.route('/').get(getAllJobs).post(validateJobInput, createJob)
 router
   .route('/:id')
   .get(validateIdParam, getJob)
-  .patch(validateJobInput, updateJob)
+  .patch(validateJobInput, validateIdParam, updateJob)
   .delete(validateIdParam, deleteJob)
 
 export default router
