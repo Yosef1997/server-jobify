04b4757685855413f0b8cf63af4bdff4182c4e96
JanBu <yosefsitumorang97@gmail.com>
2025-05-21 15:16:37 +0700

    feat:validate login input

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,24 @@
 import { StatusCodes } from 'http-status-codes'
 import User from '../models/userModel.js'
+import { hashPassword, comparePassword } from '../utils/passwordUtils.js'
+import { UnauthenticatedError } from '../errors/customErrors.js'
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0
   req.body.role = isFirstAccount ? 'admin' : 'user'
+
+  const hashedPassword = await hashPassword(req.body.password)
+  req.body.password = hashedPassword
+
   const user = await User.create(req.body)
-  res.status(StatusCodes.CREATED).json({ user })
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' })
 }
 
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email })
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password))
+  if (!isValidUser) throw new UnauthenticatedError('invalid credentials')
+
   res.send('login')
 }
